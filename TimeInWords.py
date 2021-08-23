3a944ba1b699a147a4fd8b670b67dc0fa5ded697
Abdulla Kerimov <38798208+akerimov@users.noreply.github.com>
2021-08-23 09:00:28 -0500

    Update TimeInWords.py

--- TimeInWords.py
+++ TimeInWords.py
@@ -41,7 +41,14 @@ https://www.hackerrank.com/challenges/the-time-in-words/problem
 
 def timeInWords(h, m):
     # define the dictionary: key=numerals, value = words
-    num2word = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',              6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',             11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen',             15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen',             19: 'nineteen', 20: 'twenty', 21: 'twenty one', 22: 'twenty two',             23: 'twenty three', 24: 'twenty four', 25: 'twenty five',             26: 'twenty six', 27: 'twenty seven', 28: 'twenty four',             29: 'twenty nine', 30: 'thirty', 0: 'zero'}
+    num2word = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',              
+                6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',             
+                11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen',            
+                15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen',             
+                19: 'nineteen', 20: 'twenty', 21: 'twenty one', 22: 'twenty two',            
+                23: 'twenty three', 24: 'twenty four', 25: 'twenty five',            
+                26: 'twenty six', 27: 'twenty seven', 28: 'twenty four',            
+                29: 'twenty nine', 30: 'thirty', 0: 'zero'}
     
     # check if 00 minutes return o' clock
     if m == 0:
